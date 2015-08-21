15319b6590bb589f213f43469879ac3875f82be4
Gerardo Tarragona <geratarra@gmail.com>
2015-08-21 13:57:44 -0700

    Anadido diccionario con criticas de prueba (el del libro)

---
+++ critics_dict.py
@@ -0,0 +1,22 @@
+# Diccionario de diccionarios (nested dictionary), con puntuacion que gente
+# le ha dado a ciertas peliculas
+critics = {'Tony Stark': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+                          'Just My Luck': 3.0, 'Superman Returns': 3.5,
+                          'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
+           'Emma Watson': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+                           'Just My Luck': 1.5, 'Superman Returns': 5.0,
+                           'The Night Listener': 3.0, 'You, Me and Dupree': 3.5},
+           'Dijkstra': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+                        'Superman Returns': 3.5, 'The Night Listener': 4.0},
+           'Claudia Pavlovich': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+                                 'The Night Listener': 4.5, 'Superman Returns':
+                                 4.0, 'You, Me and Dupree': 2.5},
+           'Christian Ruink': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+                               'Just My Luck': 2.0, 'Superman Returns': 3.0,
+                               'The Night Listener': 3.0, 'You, Me and Dupree':
+                               2.0},
+           'Eduardo Yeomans': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+                               'The Night Listener': 3.0, 'Superman Returns':
+                               5.0, 'You, Me and Dupree': 3.5},
+           'El Robbie': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0,
+                         'Superman Returns': 4.0}}

53c0288495032c474d89c62a29d82ee0f900dc3c
Manuel Valle <FcoManueel@gmail.com>
2015-08-30 23:19:26 -0700

    Added instructions to script when called without arguments

--- get_dataset.py
+++ get_dataset.py
@@ -7,7 +7,7 @@ FULL_SQUARE  = "█"
 EMPTY_SQUARE = " "
 BACKSPACE    = chr(8)
 TOTAL_BAR_LENGTH = 70
-DEFAULT_DATASET_URL = "http://files.grouplens.org/datasets/movielens/ml-1m.zip"
+EXAMPLE_DATASET_URL = "http://files.grouplens.org/datasets/movielens/ml-1m.zip"
 
 def download_file(file_url):
     """ Download a file from the given file_url and returns the name with which it was saved """
@@ -50,9 +50,13 @@ def write_progress_bar(downloaded_bytes, total_bytes):
 
 if __name__ == '__main__':
     """ Runs the script. If an argument is given, it's used as a url """
-    data_set_url = DEFAULT_DATASET_URL
 
-    if len(sys.argv) > 1:
+    if len(sys.argv) <= 1:
+        print "Script para descargar en la carpeta del proyecto un dataset a partir de una url"
+        print "Modo de uso: \n\t" \
+              "python -m get_dataset [url]"
+        print "Ejemplo: \n\t" \
+              'python -m get_dataset "'+EXAMPLE_DATASET_URL+'"'
+    else:
         data_set_url = sys.argv[1]
-
-    download_file(data_set_url)
+        download_file(data_set_url)

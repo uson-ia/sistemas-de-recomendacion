45d11615646ea3dc11d3a89756f1e3cb5e0e4b3c
Manuel Valle <FcoManueel@gmail.com>
2015-08-31 12:02:44 -0700

    Improved usability of loading functions
    - Now the system checks if a solicited dataset exists, and if it doesn't then it downloads it and saves it under a `dataset` folder

--- movielens.py
+++ movielens.py
@@ -1,7 +1,29 @@
 # encoding: utf-8
 
+from downloader import get_file
 import csv
+import os
 import zipfile
+
+def _file_exists(directory, filename):
+    """ Returns a bool representing if a file is present in the given directory """
+    return os.path.exists(directory) and os.path.isfile(directory+"/"+filename)
+
+def _assert_dataset_existence(directory, dataset_filename, dataset_url):
+    """ Check if a dataset exists inside the given directory. If it doesn't, offer the user to download it. Returns a bool """
+    if _file_exists(directory, dataset_filename):
+        return True
+
+    print "Parece que no tienes el dataset adecuado ("+dataset_filename+") en la carpeta '"+directory+"'"
+    should_download = (raw_input('¿Deseas descargar el dataset? [si|no]: ')).lower() == "si"
+    if not should_download:
+        print "No se descargara el archivo."
+        return False
+
+    print "Comenzará la descarga. Para cancelar, presiona control+c."
+
+    downloaded_filename = get_file(directory, dataset_url, dataset_filename)
+    return downloaded_filename != ""
 
 def _load_from_u_files(directory, filename, dataset_url):
     """ Load preference dictionary from u.item and u.data files inside a zip file (I think only 100k file uses this format) """
@@ -9,6 +31,11 @@ def _load_from_u_files(directory, filename, dataset_url):
     zip_filename = filename+".zip"
     movies_filename = filename+'/u.item'
     ratings_filename = filename+'/u.data'
+
+    # Checar si se tienen los datos
+    dataset_exists = _assert_dataset_existence(directory, zip_filename, dataset_url)
+    if not dataset_exists:
+        return None
 
     with zipfile.ZipFile(directory_path+zip_filename, "r") as z:
         # Cargar diccionario de peliculas {movieID : title}
@@ -33,6 +60,11 @@ def _load_from_csv(directory, filename, dataset_url):
     zip_filename = filename+".zip"
     movies_filename = filename+'/movies.csv'
     ratings_filename = filename+'/ratings.csv'
+
+    # Checar si se tienen los datos
+    dataset_exists = _assert_dataset_existence(directory, zip_filename, dataset_url)
+    if not dataset_exists:
+        return None
 
     with zipfile.ZipFile(directory_path+zip_filename, "r") as z:
         # Cargar diccionario de peliculas {movieID : title}

5739fa9b8bebc8d8bcf2217d4c8af2e60dd3894e
cruzluquejm <cruzluquejm@gmail.com>
2015-10-13 21:39:26 -0700

    fillItems

--- deliciousrec.py
+++ deliciousrec.py
@@ -3,6 +3,9 @@
 __author__ = 'JuanManuel'
 
 from pydelicious import get_popular, get_userposts, get_urlposts
+import random
+import recommendations
+import time
 
 def initializeUserDict(tag, count = 5):
 	user_dict = {}
@@ -15,3 +18,27 @@ def initializeUserDict(tag, count = 5):
 			user_dict[user] = {}
 
 	return user_dict
+
+def fillItems(user_dict):
+	all_items = {}
+
+	# Encuentra links posteados por otros usuarios
+	for user in user_dict:
+		for i in range(3):
+			try:
+				posts = get_userposts(user)
+				break
+			except:
+				print "Error de usuario " + user + " reintentando"
+				time.sleep(4)
+
+		for post in posts:
+			url = post['href']
+			user_dict[user][url] = 1.0
+			all_items[url] = 1
+
+	# Se rellenan los items restantes con 0
+	for ratings in user_dict.values():
+		for item in all_items:
+			if item not in ratings:
+				ratings[item] = 0.0
